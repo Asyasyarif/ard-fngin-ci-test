ba8f7658d99ebd1f29ddfa0bceeb9734608e0d4c
ARIF <asya.syarif94@gmail.com>
2026-07-16 18:39:10 +0700

    test 2

--- ard-ci-test.ino
+++ ard-ci-test.ino
@@ -36,7 +36,7 @@ void setup() {
 #if defined(ESP8266)
   fngin.setFirmwareInfo("1.0.0", "ESP8266", "esp8266:esp8266:generic");
 #elif defined(ESP32)
-  fngin.setFirmwareInfo("1.0.0", "ESP32", "esp32:esp32:esp32");
+  fngin.setFirmwareInfo("2.0.0", "ESP32", "esp32:esp32:esp32");
 #endif
 
   // OTA status callback
@@ -57,7 +57,7 @@ void loop() {
   static unsigned long lastCheck = 0;
   if (millis() - lastCheck > 2000) {
     lastCheck = millis();
-     Serial.println("OK 2 2");
+     Serial.println("OK 2 3");
     // fngin.checkOTA();
   }
 }
