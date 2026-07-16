d9a47a4d742c51dec1f43ed3c4fb41fd69dfeb98
ARIF <asya.syarif94@gmail.com>
2026-07-16 17:38:02 +0700

    test

---
+++ ard-ci-test.ino
@@ -0,0 +1,63 @@
+/*
+ * Firmngin OTA Update Example
+ *
+ * Demonstrates basic OTA firmware update using checkOTA() and performOTA()
+ *
+ * website: https://firmngin.dev
+ * author: (Arif) Firmngin.dev
+ */
+
+#include <Arduino.h>
+#include <firmngin.h>
+
+#if defined(ESP8266)
+#include <ESP8266WiFi.h>
+#elif defined(ESP32)
+#include <WiFi.h>
+#endif
+
+const char *ssid     = "PA-24G";
+const char *password = "axios123";
+
+Firmngin fngin(DEVICE_ID, DEVICE_KEY);
+
+void setup() {
+  Serial.begin(115200);
+
+  // Connect WiFi/Users/arif/Downloads/keys (42).h
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) delay(500);
+  Serial.println("WiFi connected");
+
+  // Set timezone
+  fngin.setTimezone(7);
+
+  // Set firmware info (version, board, model)
+#if defined(ESP8266)
+  fngin.setFirmwareInfo("1.0.0", "ESP8266", "esp8266:esp8266:generic");
+#elif defined(ESP32)
+  fngin.setFirmwareInfo("1.0.0", "ESP32", "esp32:esp32:esp32");
+#endif
+
+  // OTA status callback
+  fngin.onOTAStatus([](const char *status, const char *message) {
+    Serial.print("[OTA] ");
+    Serial.print(status);
+    Serial.print(": ");
+    Serial.println(message);
+  });
+
+  fngin.begin();
+}
+
+void loop() {
+  fngin.loop();
+
+  // Check for updates every 5 minutes
+  static unsigned long lastCheck = 0;
+  if (millis() - lastCheck > 2000) {
+    lastCheck = millis();
+     Serial.println("OK 2 2");
+    // fngin.checkOTA();
+  }
+}
